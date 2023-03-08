ec71d7b29d07c94e66bee06621fdfbba3073a7e2
Byeong Min Cho <merrybmc@gmail.com>
2023-03-08 23:52:36 +0900

    test:환경변수

--- src/apis/apiPOST.js
+++ src/apis/apiPOST.js
@@ -2,14 +2,11 @@ import api from './api';
 import axios from 'axios';
 
 export async function SignUp(Info) {
-  const { data } = await axios.post(
-    'http://13.209.64.59/api/user/signup',
-    Info
-  );
+  const { data } = await api.post('13.209.64.59/api/user/signup', Info);
   return data;
 }
 
 export async function SignIn(Info) {
-  const { data } = await axios.post('http://13.209.64.59/api/user/login', Info);
+  const { data } = await api.post('13.209.64.59/api/user/login', Info);
   return data;
 }
---
+++ src/store/store.js
@@ -0,0 +1,3 @@
+import { atom } from 'recoil';
+
+export const TabState = atom({ key: 'tabstate', default: '0' });
