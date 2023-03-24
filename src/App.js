b3db410d52891c4a9000e7a3837447c03228ac26
Byeong Min Cho <merrybmc@gmail.com>
2023-03-24 23:40:44 +0900

    fix : GA code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import Router from './router/Router';
 import RouteChangeTracker from './router/RouteChangeTracker';
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-function App() {
-  const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
-  ReactGA.initialize(gaTrackingId);
 
-  const history = createBrowserHistory();
-  history.listen(response => {
-    console.log(response.location.pathname);
-    ReactGA.set({ page: response.location.pathname });
-    ReactGA.pageview(response.location.pathname);
-  });
+function App() {
   RouteChangeTracker();
   return <Router />;
 }
