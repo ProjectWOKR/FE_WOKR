3557da3a7ce160086976ddde098d07b96ce0ac6e
Byeong Min Cho <merrybmc@gmail.com>
2023-03-24 23:43:29 +0900

    fix : GA code

--- src/router/RouteChangeTracker.js
+++ src/router/RouteChangeTracker.js
@@ -1,19 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
+
 const RouteChangeTracker = () => {
   const location = useLocation();
   const [initialized, setInitialized] = useState(false);
-  const gaTrackingId = process.env.GOOGLE_ANALYTICS_TARCKING_ID;
-  ReactGA.initialize(gaTrackingId);
-
-  const history = createBrowserHistory();
-  history.listen(response => {
-    console.log(response.location.pathname);
-    ReactGA.set({ page: response.location.pathname });
-    ReactGA.pageview(response.location.pathname);
-  });
 
   useEffect(() => {
     if (!window.location.href.includes('localhost')) {
